8e7f68685f60a08cb18a5cf3843745d42b946ac2
SvNelles <sv.nelles@googlemail.com>
2023-04-21 00:51:49 +0200

    Exercise 8

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,6 @@
+<script>
+const myList =["Poker","Cash","Books"];
+const myName ="Sven";
+const myOb = {first: myName, second: myList};
+</script>
+My name is {myOb.first} and these are some of my favourite things: {myOb.second} 
