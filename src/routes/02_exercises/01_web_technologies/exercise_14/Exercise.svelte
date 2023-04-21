72c8965a2a9dfa6cf480fee7b97dd8b816261b59
SvNelles <sv.nelles@googlemail.com>
2023-04-21 15:17:59 +0200

    Exercise 15

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -1,6 +1,7 @@
 <script>
   let current = "";
 </script>
+<p class:myClassName={current} style="color:red" />
 
 <!-- We will cover buttons later on! -->
 <button class="btn btn-secondary" on:click={() => (current = "messi")}>
--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -10,6 +10,8 @@
     <g transform="translate({margin.left}, {margin.top})">
       <!--  -->
     </g>
+    <rect x=30 y=30 width=40 height=40 color="yellow"/>
+    <circle cx=70 cy=70 r=20 />
   </svg>
   
   <style>
@@ -17,5 +19,11 @@
       border: 1px solid black;
       border-radius: 5px;
     }
+            rect {
+            fill: yellow;
+          }
+                  circle {
+            fill: skyblue;
+        }
   </style>
   
