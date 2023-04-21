73e93eb74e6c4a6e1863fbbfa4a382cb53be6001
SvNelles <sv.nelles@googlemail.com>
2023-04-21 14:24:19 +0200

    Exercise 10

---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,12 @@
+<script>
+import Ex2 from "/home/SvNelles/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte";
+import Ex3 from "/home/SvNelles/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte"
+</script>
+
+<div>
+<Ex3/>
+</div>
+
+<div>
+<Ex2/>
+</div>
