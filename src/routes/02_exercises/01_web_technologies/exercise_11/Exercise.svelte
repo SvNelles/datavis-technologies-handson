dfaac22a996e6b86448b34b4054aedecf5fd8c42
SvNelles <sv.nelles@googlemail.com>
2023-04-21 14:45:31 +0200

    Exercise 11

---
+++ src/routes/02_exercises/01_web_technologies/exercise_11/Exercise.svelte
@@ -0,0 +1,5 @@
+<script>
+import Child from "/home/SvNelles/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_11/Exercise_child.svelte";
+</script>
+
+<p>answer to life the universe and everything is <Child/> </p>
